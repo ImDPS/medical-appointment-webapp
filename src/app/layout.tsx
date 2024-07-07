2f85b1818e3ffab3c65496066c438b0ab8c3944b
Durgendra <dpsmad999@gmail.com>
2024-07-08 03:44:33 +0530

    jakarta font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
-const fontJakarta = Plus_Jakarta_Sans({ 
+const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-jakarta",
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -22,9 +23,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body 
-        className={cn('min-h-screen bg-dark-300 font-jakarta antialiased', fontJakarta.variable)}
+        className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
